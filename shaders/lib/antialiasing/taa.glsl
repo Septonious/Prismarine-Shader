b9b36ed52379b93f6fd0aeb0419107d374f0bf07
Septonious <83358692+Septonious@users.noreply.github.com>
2022-01-07 12:39:27 +0300

    Update taa.glsl

--- shaders/lib/antialiasing/taa.glsl
+++ shaders/lib/antialiasing/taa.glsl
@@ -93,7 +93,7 @@ vec4 TemporalAA(inout vec3 color, float tempData, sampler2D colortex, sampler2D 
 		prvCoord.x > 0.0 && prvCoord.x < 1.0 &&
 		prvCoord.y > 0.0 && prvCoord.y < 1.0
 	);
-	blendFactor *= exp(-length(velocity));
+	blendFactor *= exp(-length(velocity)) * 0.6 + 0.3;
 	
 	color = mix(color, tempColor, blendFactor);
 	return vec4(tempData, color);
