a889d8d5400cb1743dd316b817ed9711b4a1c1f5
Septonious <83358692+Septonious@users.noreply.github.com>
2022-01-23 16:52:30 +0300

    Update composite5.glsl

--- shaders/program/composite5.glsl
+++ shaders/program/composite5.glsl
@@ -222,7 +222,7 @@ void main() {
 	
 	gl_Position = ftransform();
 
-	#ifdef LENS_FLARE
+	#if defined BLOOM || defined LENS_FLARE
 	const vec2 sunRotationData = vec2(cos(sunPathRotation * 0.01745329251994), -sin(sunPathRotation * 0.01745329251994));
 	float ang = fract(timeAngle - 0.25);
 	ang = (ang + (cos(ang * 3.14159265358979) * -0.5 + 0.5 - ang) / 3.0) * 6.28318530717959;
