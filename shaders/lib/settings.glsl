c6a0dc291c706831ee0c0c182d3ba41bf60ce707
Septonious <83358692+Septonious@users.noreply.github.com>
2022-05-12 20:20:49 +0300

    edit: fuck

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -25,7 +25,7 @@ Read lang/en_US.lang to get the description of what every option does.
 Please don't edit anything from Undefine section and onwards.
 */
 
-#define VOLUMETRICS_RENDER_RESOLUTION 0.5 //[0.5 0.6 0.7 0.8 0.9 1.0]
+#define VOLUMETRICS_RENDER_RESOLUTION 0.7 //[0.5 0.6 0.7 0.8 0.9 1.0]
 #define TAA_JITTER
 
 ///////////////////////////////////////////////// P R I S M A R I N E /////////////////////////////////////////////////
@@ -81,9 +81,9 @@ Please don't edit anything from Undefine section and onwards.
 
 #define WATER_LIGHTSHAFTS
 #define LIGHTSHAFT_CLOUDY_NOISE
-#define LIGHTSHAFT_SAMPLES 10 //[1 2 3 4 5 6 7 8 10]
+#define LIGHTSHAFT_SAMPLES 8 //[1 2 3 4 5 6 7 8 10]
 #define LIGHTSHAFT_MAX_DISTANCE 256.0 //[128.0 256.0 512.0 1024.0]
-#define LIGHTSHAFT_MIN_DISTANCE 4.0 //[1.0 2.0 4.0 8.0 12.0 16.0 24.0]
+#define LIGHTSHAFT_MIN_DISTANCE 8.0 //[1.0 2.0 4.0 8.0 12.0 16.0 24.0]
 #define LIGHTSHAFT_HEIGHT 8 //[10 9 8 7 6 5 4 3 2 1]
 
 #define VCLOUDS_OCTAVES 3 //[1 2 3 4 5]
